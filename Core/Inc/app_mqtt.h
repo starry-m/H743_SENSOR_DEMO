800c4dcff44f2f6a3011bf99dfc105135a0d6f88
ming <2386279494@qq.com>
2024-06-13 01:54:06 +0800

    add mqtt client

---
+++ Core/Inc/app_mqtt.h
@@ -0,0 +1,21 @@
+/*
+ * app_mqtt.h
+ *
+ *  Created on: Jun 12, 2024
+ *      Author: pc
+ */
+
+#ifndef INC_APP_MQTT_H_
+#define INC_APP_MQTT_H_
+
+
+/*!
+* @brief 封装 MQTT 初始化接口
+*        执行条件：无
+*
+* @retval: 无
+*/
+void bsp_mqtt_init(void);
+
+
+#endif /* INC_APP_MQTT_H_ */
